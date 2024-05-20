9389500c6702488e65fa679c68f255dcda3743fe
José Miguel Villacorta Arteaga <jvillacorta@ucp.edu.pe>
2024-05-19 23:29:49 -0500

    first commit create project template to main.
    first commit create project template to main.

---
+++ EncuestadoraDigitalia/EncuestadoraDigitalia.Core/Services/Shop/Interfaces/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------
+// Email: quickapp@ebenmonney.com
+// Templates: www.ebenmonney.com/templates
+// (c) 2024 www.ebenmonney.com/mit-license
+// ---------------------------------------
+
+using EncuestadoraDigitalia.Core.Models.Shop;
+
+namespace EncuestadoraDigitalia.Core.Services.Shop
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetTopActiveCustomers(int count);
+        IEnumerable<Customer> GetAllCustomersData();
+    }
+}
